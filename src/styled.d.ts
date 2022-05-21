752f0bb4fca9625379b8ce7d9794b5c4d4e6d9fb
Danim Kim <kdn725@gmail.com>
2022-05-22 01:17:46 +0900

    💄  declare theme colors

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,9 +1,16 @@
-import 'styled-components';
+import "styled-components";
 
-// and extend them!
-declare module 'styled-components' {
+declare module "styled-components" {
   export interface DefaultTheme {
-    textColor: string;
-    bgColor: string;
-  };
+    red: string;
+    black: {
+      veryDark: string;
+      darker: string;
+      lighter: string;
+    };
+    white: {
+      darker: string;
+      lighter: string;
+    };
+  }
 }
