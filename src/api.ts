fed266c557dadc5e1ff9f98e628b00f9768e8cb6
Danim Kim <kdn725@gmail.com>
2022-05-22 01:25:45 +0900

    ✨ create API utils

---
+++ src/api.ts
@@ -0,0 +1,27 @@
+const API_KEY = 'ba70c390de4655e9260f08d4ffa9a63f';
+const BASE_PATH = 'https://api.themoviedb.org/3';
+
+export interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export function getMovies() {
+  return fetch(
+    `${BASE_PATH}/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`,
+  ).then((response) => response.json());
+}
